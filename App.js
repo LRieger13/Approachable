518a17d552c2a932655f5598a141d3e92f7d27a4
Leah Rieger <leastuart92@gmail.com>
2021-12-21 12:04:57 -0800

    State
    Got state to save on the home page based on changes in text input.
    
    Next: get state to save from edit page to home page;
    
    Use icons for save & cancel if possible;

--- App.js
+++ App.js
@@ -1,27 +1,32 @@
-import { StyleSheet, Text, TextInput, View, Image, Button, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import React, {useState} from 'react';
-import { withSafeAreaInsets } from 'react-native-safe-area-context';
-
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import React, { useState } from "react";
+import { withSafeAreaInsets } from "react-native-safe-area-context";
 
 const Stack = createStackNavigator();
 
 /// HOME
-function Home({navigation}) {
- 
-  const [text, setText] = useState('');
+function Home({ navigation }) {
+  const [text, setText] = useState("");
   const [info, setInfo] = useState([]);
-
 
   const inputHandler = (text) => {
     setText(text);
   };
 
   const addInfo = () => {
-    setInfo(currentInfo => [...info, text]);
-  }
+    setInfo((currentInfo) => [...info, text]);
+  };
 
   return (
     <View style={styles.container}>
@@ -31,42 +36,86 @@ function Home({navigation}) {
       {/* Add ability to click small icon to upload own picture */}
       <MaterialIcons name="add-a-photo" size={15} style={styles.addPhoto} />
       <br />
+      
+
       {/* INFORMATION :: each leads to a redirect to edit that section info */}
-      <TouchableOpacity style={{backgroundColor: '#fff', padding:50, width: '30%', borderRadius: 5, flexDirection: 'row', justifyContent:'space-between'}}>
-        <Text>Names</Text>      {/* Display name that is edited */}
-        <MaterialIcons name="arrow-forward-ios" size={20} color="black" onPress={() => navigation.navigate('Edit')} />
+      {/* JOIN FIRST & LAST NAME FOR OUTPUT */}
+      {/*  */}
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#fff",
+          padding: 50,
+          width: "30%",
+          borderRadius: 5,
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Text>{text}</Text> {/* Display name that is edited */}
+        <MaterialIcons
+          name="arrow-forward-ios"
+          size={20}
+          color="black"
+          onPress={() => navigation.navigate("Edit")}
+        />
       </TouchableOpacity>
       <br />
-      <TextInput placeholder="Name" onChangeText={inputHandler} value={text}></TextInput>
+
+      {/* LEARNING PASSING STATE */}
+      <TextInput
+        placeholder="Name"
+        onChangeText={inputHandler}
+        value={text}
+      ></TextInput>
       <Button title="Edit" onPress={addInfo} />
       <View>
-        {info.map((texts) => <Text key={texts}>{texts}</Text>)}
+        {info.map((texts) => (
+          <Text key={texts}>{texts}</Text>
+        ))}
       </View>
     </View>
   );
 }
 
 /// EDIT
-function Edit({navigation}) {
+function Edit({ navigation }) {
+  {
+    /* STATE */
+  }
 
+  {
+    /*  */
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Edit</Text>
       <br />
-      <TextInput placeholder="First Name"style={styles.edit}></TextInput>
-      <TextInput placeholder="Last Name" style={styles.edit} onChangeText={setText}></TextInput>
+      {/* 2 TEXT INPUTS
+          1 FOR FNAME, 1 FOR LNAME */}
+      <TextInput placeholder="First Name" style={styles.edit}></TextInput>
+      <TextInput
+        placeholder="Last Name"
+        style={styles.edit}
+      ></TextInput>
       <br />
-      <Button title="Save" onPress={() => navigation.navigate('Home')} />
+      {/* ON SAVE BUTTON NEEDS TO GO BACK TO HOME SCREEN & MOVE CHANGED STATE */}
+      {/* JOIN FNAME\LNAME HERE OR ON HOME SCREEN? */}
+      {/* USE ICON TO SAVE OR CANCEL */}
+      <MaterialIcons
+          name="check"
+          size={20}
+          color="black"
+        />
+      <Button title="Cancel" onPress={() => navigation.navigate('Home')} />
     </View>
   );
 }
-
 
 /// MAIN APP
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Edit" component={Edit} />
       </Stack.Navigator>
@@ -82,7 +131,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
- 
+
   profile: {
     alignItems: "center",
     borderRadius: 50,
@@ -93,27 +142,25 @@ const styles = StyleSheet.create({
     backgroundColor: "#00F0FF",
     resizeMode: "contain",
   },
- 
+
   edit: {
     justifyContent: "space-between",
     flexDirection: "row",
     padding: 20,
-    border: 5, 
-    borderColor: "red"
+    border: 5,
+    borderColor: "red",
   },
 
   addPhoto: {
-    position: 'absolute',
     justifyContent: "center",
-    zIndex: 1
+    alignItems: "center",
+    paddingLeft: 50,
   },
 
   title: {
     fontSize: 50,
     fontWeight: "bold",
     marginBottom: 50,
-    marginTop: -20
-  }
- 
+    marginTop: -20,
+  },
 });
-
